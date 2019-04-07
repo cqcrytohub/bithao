f89738e77f8f3878ed6437a140412addf5e8d3f2
bithao <bithao>
2019-04-07 20:36:29 +0900

    xx

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -163,7 +163,7 @@ public:
 
         chainTxData = ChainTxData{
             // Data as of block 59c9b9d3fec105bdc716d84caa7579503d5b05b73618d0bf2d5fa639f780a011 (height 1353397).
-            1516406833, // * UNIX timestamp of last known number of transactions
+            1546300800, // * UNIX timestamp of last known number of transactions
             0,  // * total number of transactions between genesis and that timestamp
                     //   (the tx=... number in the SetBestChain debug.log lines)
             0.06     // * estimated number of transactions per second after that timestamp
