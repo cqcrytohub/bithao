f12254b7bb5760f8de2bf7cebd1d58f361ef01f3
bithao <bithao>
2019-04-08 10:51:35 +0900

    xx

--- src/pow.cpp
+++ src/pow.cpp
@@ -22,27 +22,35 @@ unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHead
         return nProofOfWorkLimit;
     }
     // Only change once per difficulty adjustment interval
-    if ((pindexLast->nHeight+1) % params.DifficultyAdjustmentInterval() != 0)
-    {
-        if (params.fPowAllowMinDifficultyBlocks)
-        {
-            // Special difficulty rule for testnet:
-            // If the new block's timestamp is more than 2* 10 minutes
-            // then allow mining of a min-difficulty block.
-            if (pblock->GetBlockTime() > pindexLast->GetBlockTime() + params.nPowTargetSpacing*2)
-                return nProofOfWorkLimit;
-            else
-            {
-                // Return the last non-special-min-difficulty-rules-block
-                const CBlockIndex* pindex = pindexLast;
-                while (pindex->pprev && pindex->nHeight % params.DifficultyAdjustmentInterval() != 0 && pindex->nBits == nProofOfWorkLimit)
-                    pindex = pindex->pprev;
-                return pindex->nBits;
-            }
-        }
-        return pindexLast->nBits;
-    }
+    // if ((pindexLast->nHeight+1) % params.DifficultyAdjustmentInterval() != 0)
+    // {
+    //     if (params.fPowAllowMinDifficultyBlocks)
+    //     {
+    //         // Special difficulty rule for testnet:
+    //         // If the new block's timestamp is more than 2* 10 minutes
+    //         // then allow mining of a min-difficulty block.
+    //         if (pblock->GetBlockTime() > pindexLast->GetBlockTime() + params.nPowTargetSpacing*2)
+    //             return nProofOfWorkLimit;
+    //         else
+    //         {
+    //             // Return the last non-special-min-difficulty-rules-block
+    //             const CBlockIndex* pindex = pindexLast;
+    //             while (pindex->pprev && pindex->nHeight % params.DifficultyAdjustmentInterval() != 0 && pindex->nBits == nProofOfWorkLimit)
+    //                 pindex = pindex->pprev;
+    //             return pindex->nBits;
+    //         }
+    //     }
+    //     return pindexLast->nBits;
+    // }
 
+     //too fast
+    if (pblock->GetBlockTime() <  ( pindexLast->GetBlockTime() +  params.nPowTargetSpacing/3)){
+        unsigned int ret =pindexLast->nBits / 2;
+        if(fDebug)
+            LogPrint("mine", "prevhieght:%d too fast block %08x\n ",pindexLast->nHeight, ret);
+        return ret;
+    }
+    
     // Go back by what we want to be 14 days worth of blocks
     // Bithao: This fixes an issue where a 51% attack can change difficulty at will.
     // Go back the full period unless it's the first retarget after genesis. Code courtesy of Art Forz
@@ -52,8 +60,12 @@ unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHead
 
     // Go back by what we want to be 14 days worth of blocks
     const CBlockIndex* pindexFirst = pindexLast;
-    for (int i = 0; pindexFirst && i < blockstogoback; i++)
+    for (int i = 0; pindexFirst && i < blockstogoback; i++){
+        if(pindexFirst->nHeight<=1)//root
+            break;
         pindexFirst = pindexFirst->pprev;
+    }
+        
 
     assert(pindexFirst);
 
